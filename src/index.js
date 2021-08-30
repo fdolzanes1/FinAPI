9584fbfcbb4d9c1f8df091cc94f9bb1c287d5f02
fdolzanes1 <fdolzanes1@gmail.com>
2021-08-30 19:14:12 -0400

    cadastro e validacao de conta

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const { v4:uuidv4 } = require('uuid'); 
 app.use(morgan());
-app.use('/', (req,res) => {
-  res.status(200).send({ message: "Hello" });
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).send({ error: "Customer Already Exists!"});
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+  return res.status(201).send({ message: "Ok" });
 });
+
+
 app.listen(3000);
